26467f49168fc84abf95328bfe35fa5ef45daa13
Pietromuresu <muresupietro66@gmail.com>
2023-04-12 16:49:57 +0200

    Replace the current h1 with the new message

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.             <br>
 ###  **Bonus:**        <br>
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+### Funzionamento: 
+l'utente inserisce un testo a scelta e il titolo principale viene sostituito da quello scritto dall'utente quando il tasto 'enter' viene premuto
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,16 @@
 const {createApp} = Vue;
 
 createApp({
-
   data(){
     return {
-      messaggio: 'Ciao sostituiscimi con un messaggio',
+      messaggio: 'inserisci un nuovo messaggio',
+      newMessage: '',
       foto: '../assets/img/mountain-view-google.avif'
     }
+  },
+  methods: {
+    printNewMessage() {
+      this.messaggio = this.newMessage
+    }
   }
-
 }).mount('#app')
-
-
